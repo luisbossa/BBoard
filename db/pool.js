6ffbd4d2edd7ef768872c0f3051d61ae70c6ea2d
luisbossa <luisbossa@gmail.com>
2026-01-17 15:45:01 -0600

    prod

--- db/pool.js
+++ db/pool.js
@@ -1,21 +1,20 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD ?? "",
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // (async () => {
 //   try {
 //     const client = await pool.connect();
 //     const res = await client.query("SELECT NOW()");
-//     console.log("✅ PostgreSQL conectado:", res.rows[0]);
+//     console.log("PostgreSQL conectado:", res.rows[0]);
 //     client.release();
 //   } catch (err) {
-//     console.error("❌ Error conectando a PostgreSQL:", err.message);
+//     console.error("Error conectando a PostgreSQL:", err.message);
 //   }
 // })();
 
