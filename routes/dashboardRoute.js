58142fe2e88ffec2ddfcd86ee26aa606370a7c06
luisbossa <luisbossa@gmail.com>
2026-01-19 00:44:45 -0600

    ui dash

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
-// Importa el middleware de autenticación
 const authenticateToken = require("../middlewares/auth");
 const notificationService = require("../services/notificationService");
 
-// Ruta protegida para el Dashboard (solo usuarios autenticados)
 router.get("/", authenticateToken, dashboardController.dashboard);
 router.put(
   "/orders/:id/approve",
