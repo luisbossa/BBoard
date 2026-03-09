aa0821dcc134f70ae3854f40a1e132d75d880a90
luisbossa <luisbossa@gmail.com>
2026-03-08 19:33:03 -0600

    pg

--- db/pool.js
+++ db/pool.js
@@ -1,37 +1,38 @@
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// module.exports = pool;
-
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
 
-(async () => {
-  try {
-    const client = await pool.connect();
-    const res = await client.query("SELECT NOW()");
-    console.log("PostgreSQL conectado:", res.rows[0]);
-    client.release();
-  } catch (err) {
-    console.error("Error conectando a PostgreSQL:", err.message);
-  }
-})();
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+//   ssl: false
+// });
+
+// module.exports = pool;
+
+// (async () => {
+//   try {
+//     const client = await pool.connect();
+//     const res = await client.query("SELECT NOW()");
+//     console.log("PostgreSQL conectado:", res.rows[0]);
+//     client.release();
+//   } catch (err) {
+//     console.error("Error conectando a PostgreSQL:", err.message);
+//   }
+// })();
+
 
 
 
